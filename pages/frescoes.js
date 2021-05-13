9c34ce21e03750b5f0691adca465227665cc7067
Joshua Vayer <joshua.vayer@outlook.com>
2021-05-13 15:25:55 +0200

    AS_FR#J.20 - Frescoes template page

--- pages/frescoes.js
+++ pages/frescoes.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Layout from '../components/Layout';
 
 export default function Frescoes() {
+  const entries = ['Project 1', 'Project 2', 'Project 3', 'Project 4'];
   return (
     <Layout>
 
@@ -9,8 +10,23 @@ export default function Frescoes() {
         <title>Anne Schouler - Frescoes</title>
       </Head>
 
-      <div>
-          Frescoes works
+      <div className="content">
+        <h4 className="content-title">Interior design work</h4>
+        <div className="categories categories__wrap">
+
+          {/* Cards generation */}
+          {entries.map( (p) => (
+            <div className="category category__header">
+              <div className="category-title ">{p} - Location </div>
+              <a href="click.html" className="category-image">
+                <img src="https://via.placeholder.com/575x647.png" alt="Category" />
+              </a>
+            </div>
+          ))}
+
+          {/* Last card for the to send last real flexbox element to the left */}
+          <div className="category category__last"></div>
+        </div>
       </div>
     </Layout>
   );
