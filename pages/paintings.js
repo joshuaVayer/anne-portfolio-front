817d0253c59a0228359fdfb8328416231463d638
Joshua Vayer <joshua.vayer@outlook.com>
2021-05-13 15:04:28 +0200

    AS_FR#J.19 - Painting sub-categories

--- pages/paintings.js
+++ pages/paintings.js
@@ -7,11 +7,30 @@ export default function Painting() {
 
       <Head>
         <title>Anne Schouler - Paintings</title>
-        <link rel="stylesheet" href="/css/center.css"></link>
       </Head>
 
-      <div>
-          Paintings works
+      <div className="content content__center">
+        <h4 className="content-title">Paintings</h4>
+        <div className="categories">
+          <div className="category category__reverse">
+            <div className="category-title ">Interior paintings</div>
+            <a href="/paintings/interior" className="category-image">
+              <img src="https://via.placeholder.com/575x647.png" alt="Category" />
+            </a>
+          </div>
+          <div className="category category__reverse">
+            <div className="category-title">Abstract</div>
+            <a href="/paintings/abstract" className="category-image">
+              <img src="https://via.placeholder.com/575x647.png" alt="Category" />
+            </a>
+          </div>
+          <div className="category category__reverse">
+            <div className="category-title">Portraits</div>
+            <a href="/paintings/portraits" className="category-image">
+              <img src="https://via.placeholder.com/575x647.png" alt="Category" />
+            </a>
+          </div>
+        </div>
       </div>
     </Layout>
   );
