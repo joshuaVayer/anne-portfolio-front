f0a3efd92c572dec12b3b8ea66ebdce3ace386e3
Joshua Vayer <joshua.vayer@outlook.com>
2021-05-17 14:35:57 +0200

    AS_FR#J.57 - Categories images added

--- pages/index.js
+++ pages/index.js
@@ -15,19 +15,19 @@ export default function Home() {
           <div className="category">
             <div className="category-title">Interior Design</div>
             <a href="/interior-design" className="category-image">
-              <img src="https://via.placeholder.com/575x647.png" alt="Category" />
+              <img src="https://res.cloudinary.com/dxaa1zmcg/image/upload/v1621248941/Anne/st_regis_prsidential_suite_me9r7t.jpg" alt="Interior Design" />
             </a>
           </div>
           <div className="category">
             <div className="category-title">Frescoes</div>
             <a href="/frescoes" className="category-image">
-              <img src="https://via.placeholder.com/575x647.png" alt="Category" />
+              <img src="https://res.cloudinary.com/dxaa1zmcg/image/upload/v1621248941/Anne/patio_Gerry_ynqj81.jpg" alt="Frescoes" />
             </a>
           </div>
           <div className="category">
             <div className="category-title">Paintings</div>
             <a href="/paintings" className="category-image">
-              <img src="https://via.placeholder.com/575x647.png" alt="Category" />
+              <img src="https://res.cloudinary.com/dxaa1zmcg/image/upload/v1621248894/Anne/painting_1_vhmg51.jpg" alt="Paintings" />
             </a>
           </div>
         </div>
--- pages/paintings.js
+++ pages/paintings.js
@@ -15,19 +15,19 @@ export default function Painting() {
           <div className="category category__reverse">
             <div className="category-title ">Interior paintings</div>
             <a href="/paintings/interior" className="category-image">
-              <img src="https://via.placeholder.com/575x647.png" alt="Category" />
+              <img src="https://res.cloudinary.com/dxaa1zmcg/image/upload/v1621248894/Anne/interior_painting_dvqmjp.jpg" alt="Category" />
             </a>
           </div>
           <div className="category category__reverse">
             <div className="category-title">Abstract</div>
             <a href="/paintings/abstract" className="category-image">
-              <img src="https://via.placeholder.com/575x647.png" alt="Category" />
+              <img src="https://res.cloudinary.com/dxaa1zmcg/image/upload/v1621248894/Anne/painting_1_vhmg51.jpg" alt="Category" />
             </a>
           </div>
           <div className="category category__reverse">
             <div className="category-title">Portraits</div>
             <a href="/paintings/portrait" className="category-image">
-              <img src="https://via.placeholder.com/575x647.png" alt="Category" />
+              <img src="https://res.cloudinary.com/dxaa1zmcg/image/upload/v1621248894/Anne/portrait_ijbxfg.jpg" alt="Category" />
             </a>
           </div>
         </div>
